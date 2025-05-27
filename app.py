ae4306c1be3c63b537e905d515e412e340962784
Dhiraj Singh <dhiraj.singh@Dhirajs-MacBook-Air.local>
2025-05-27 17:10:36 +0530

    :-:

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 def analyze_sentiment(text):
     if "good" in text.lower():
-        return "Positivee"
+        return "Positive"
     else:
-        return "Negativee"
+        return "Negative"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
